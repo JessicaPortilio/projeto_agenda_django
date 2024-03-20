040e0d17c381df09f444465a953686bacc467940
Jessica <jessicaportilio@academico.ufs.br>
2024-03-20 12:28:19 -0300

    Criando arquivos parciais para usar como include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -10,7 +10,8 @@ def index(request):
     print(contacts.query)
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
     return render(
         request,
@@ -29,8 +30,11 @@ def contact(request, contact_id):
         Contact.objects.filter(pk=contact_id, show = True)
         )
 
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} '
+
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': contact_name
     }
     return render(
         request,
